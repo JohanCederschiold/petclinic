8f2c4ea7f25cf13e814d5a1072c0a90d2fa2853c
JohanCederschiold <johan.cederschiold@gmail.com>
2019-11-15 14:22:06 +0100

    Implemented map based services. Closes #6

---
+++ pets-data/src/main/java/se/jpdc/pets/service/map/VetServiceMap.java
@@ -0,0 +1,34 @@
+package se.jpdc.pets.service.map;
+
+import se.jpdc.pets.model.Vet;
+import se.jpdc.pets.service.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        super.delete(vet);
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return super.save(vet.getId(), vet);
+    }
+
+    @Override
+    public void deleteByID(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+}
