4594096a45a855cfd90eed63da001393a73ffb13
JohanCederschiold <johan.cederschiold@gmail.com>
2019-12-02 14:06:54 +0100

    Vets now presented on index page. Closes #15

--- pets-web/src/main/java/se/jpdc/pets/controllers/VetController.java
+++ pets-web/src/main/java/se/jpdc/pets/controllers/VetController.java
@@ -1,13 +1,22 @@
 package se.jpdc.pets.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import se.jpdc.pets.service.VetService;
 
 @Controller
 public class VetController {
 
+    private final VetService service;
+
+    public VetController(VetService service) {
+        this.service = service;
+    }
+
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", service.findAll());
         return "vets/index";
     }
 }
