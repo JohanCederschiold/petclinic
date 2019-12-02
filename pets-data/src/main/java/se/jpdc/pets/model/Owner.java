bb683a81af5d39fcb3714592ba33a646af256a17
JohanCederschiold <johan.cederschiold@gmail.com>
2019-12-02 13:51:05 +0100

    Now presenting owners on indexpage. Closes #16

--- pets-data/src/main/java/se/jpdc/pets/model/Owner.java
+++ pets-data/src/main/java/se/jpdc/pets/model/Owner.java
@@ -1,4 +1,12 @@
 package se.jpdc.pets.model;
 
 public class Owner extends Person {
+
+    public String getFirstName() {
+        return super.getFirstName();
+    }
+
+    public String getLastName() {
+        return super.getLastName();
+    }
 }
--- pets-web/src/main/java/se/jpdc/pets/controllers/OwnerController.java
+++ pets-web/src/main/java/se/jpdc/pets/controllers/OwnerController.java
@@ -1,14 +1,25 @@
 package se.jpdc.pets.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import se.jpdc.pets.service.OwnerService;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+
+    private final OwnerService service;
+
+    public OwnerController(OwnerService service) {
+        this.service = service;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+        model.addAttribute("owners", service.findAll());
         return "owners/index";
     }
 }
